a995cc8c52e56e6f7e57ebc6ae82c6a3d574d3f4
chLi <565353780@qq.com>
2022-07-24 16:48:50 +0800

    add signal op

---
+++ Method/signal.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from Method.path import createFileFolder
+
+def sendSignal(signal_file_path, signal_data="1"):
+    createFileFolder(signal_file_path)
+
+    with open(signal_file_path, "w") as f:
+        f.write(signal_data)
+    return True
+
+def getSignal(signal_file_path):
+    while not os.path.exists(signal_file_path):
+        continue
+
+    data = None
+    with open(signal_file_path, "r") as f:
+        data = f.read()
+
+    os.remove(signal_file_path)
+    return data
+
