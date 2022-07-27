a34fb5079b073e558af0dbf661e040c6760a537e
chLi <565353780@qq.com>
2022-07-27 19:35:40 +0800

    create simple demo

---
+++ client.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Module.max_client import demo
+
+if __name__ == "__main__":
+    demo()
+
--- demo.py
+++ demo.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Module.max_client import demo
+from Module.max_client import MaxClient
 
 if __name__ == "__main__":
-    demo()
+    server_ip = "192.168.2.16"
+    server_port = 9365
+
+    #  max_file_path = "C:/Program Files/Autodesk/3ds Max 2023/presets/Particle Flow/earth_Squib Sand01.max"
+    max_file_path = "/home/chli/chLi/tmp.max"
+    save_obj_file_path = "/home/chli/chLi/test.obj"
+
+    max_client = MaxClient(server_ip, server_port)
+    max_client.transMaxToObj(max_file_path, save_obj_file_path)
+
+    #  max_client.stop()
 
