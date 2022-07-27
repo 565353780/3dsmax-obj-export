927479fc4285cfa9c395c93fdb63b945f06bf83d
chLi <565353780@qq.com>
2022-07-27 12:33:02 +0800

    simplify getObject algo, remove much useless repeated func

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pymxs
 from pymxs import runtime as rt
 
 class MaxOp(object):
@@ -9,6 +10,8 @@ class MaxOp(object):
         self.object_names = []
         self.selection = []
         self.selection_names = []
+
+        self.update()
         return
 
     def update(self):
@@ -36,34 +39,18 @@ class MaxOp(object):
             self.update()
         return True
 
-    def selectObject(self, obj, update=True):
-        rt.select(obj)
-        if update:
-            self.update()
-        return True
-
-    def selectObjectByIdx(self, object_idx, update=True):
-        if object_idx >= len(self.objects):
-            print("[ERROR][MaxOp::selectObjectByIdx]")
-            print("\t object_idx out of range!")
+    def selectObject(self, object_info, with_history=True, update=True):
+        obj = self.getObject(object_info)
+        if obj is None:
+            print("[ERROR][MaxOp::selectObject]")
+            print("\t getObject failed!")
             return False
 
-        self.selectObject(self.objects[object_idx], False)
-        if update:
-            self.update()
-        return True
+        if with_history:
+            rt.select([obj] + self.selection)
+        else:
+            rt.select(obj)
 
-    def selectObjectByName(self, object_name, update=True):
-        if object_name not in self.object_names:
-            print("[ERROR][MaxOp::selectObjectByName]")
-            print("\t object_name not exist!")
-            return False
-
-        object_idx = self.object_names.index(object_name)
-        if not self.selectObjectByIdx(object_idx, False):
-            print("[ERROR][MaxOp::selectObjectByName]")
-            print("\t selectObjectByIdx failed!")
-            return False
         if update:
             self.update()
         return True
@@ -74,34 +61,14 @@ class MaxOp(object):
             self.update()
         return True
 
-    def deSelectObject(self, obj, update=True):
+    def deSelectObject(self, object_info, update=True):
+        obj = self.getObject(object_info)
+        if obj is None:
+            print("[ERROR][MaxOp::deSelectObject]")
+            print("\t getObject failed!")
+            return False
+
         rt.deselect(obj)
-        if update:
-            self.update()
-        return True
-
-    def deSelectObjectByIdx(self, object_idx, update=True):
-        if object_idx >= len(self.objects):
-            print("[ERROR][MaxOp::deSelectObjectByIdx]")
-            print("\t object_idx out of range!")
-            return False
-
-        self.deSelectObject(self.objects[object_idx], False)
-        if update:
-            self.update()
-        return True
-
-    def deSelectObjectByName(self, object_name, update=True):
-        if object_name not in self.object_names:
-            print("[ERROR][MaxOp::selectObjectByName]")
-            print("\t object_name not exist!")
-            return False
-
-        object_idx = self.object_names.index(object_name)
-        if not self.deSelectObjectByIdx(object_idx, False):
-            print("[ERROR][MaxOp::deSelectObjectByName]")
-            print("\t deSelectObjectByIdx failed!")
-            return False
         if update:
             self.update()
         return True
@@ -112,20 +79,78 @@ class MaxOp(object):
             self.update()
         return True
 
-    def chooseObject(self, update=True):
-        if not self.deSelectAll(False):
-            print("[ERROR][MaxOp::chooseObject]")
-            print("\t deSelectAll failed!")
+    def deleteObject(self, object_info, update=True):
+        obj = self.getObject(object_info)
+        if obj is None:
+            print("[ERROR][MaxOp::deleteObject]")
+            print("\t getObject failed!")
             return False
 
+        rt.delete(obj)
         if update:
             self.update()
         return True
 
     def deleteAll(self, update=True):
         rt.delete(rt.objects)
-        self.update()
+        if update:
+            self.update()
         return True
+
+    def getObjectByIdx(self, object_idx):
+        if object_idx >= len(self.objects):
+            print("[ERROR][MaxOp::getObjectByIdx]")
+            print("\t object_idx out of range!")
+            return None
+        return self.objects[object_idx]
+
+    def getObjectIdxByName(self, object_name):
+        if object_name not in self.object_names:
+            print("[ERROR][MaxOp::getObjectIdxByName]")
+            print("\t object_name not exist!")
+            return None
+
+        object_idx = self.object_names.index(object_name)
+        return object_idx
+
+    def getObjectByName(self, object_name):
+        object_idx = self.getObjectIdxByName(object_name)
+        if object_idx is None:
+            print("[ERROR][MaxOp::getObjectByName]")
+            print("\t getObjectIdxByName failed!")
+            return None
+
+        obj = self.getObjectByIdx(object_idx)
+        if obj is None:
+            print("[ERROR][MaxOp::getObjectByName]")
+            print("\t getObjectByIdx failed!")
+            return None
+
+        return obj
+
+    def getObject(self, object_info):
+        if isinstance(object_info, str):
+            obj = self.getObjectByName(object_info)
+            if obj is None:
+                print("[ERROR][MaxOp::getObject]")
+                print("\t getObjectByName failed!")
+                return None
+            return obj
+
+        if isinstance(object_info, int):
+            obj = self.getObjectByIdx(object_info)
+            if obj is None:
+                print("[ERROR][MaxOp::getObject]")
+                print("\t getObjectByIdx failed!")
+                return None
+            return obj
+
+        if isinstance(object_info, pymxs.MXSWrapperBase):
+            return object_info
+
+        print("[ERROR][MaxOp::getObject]")
+        print("\t object_info not valid!")
+        return None
 
     def getNames(self, object_list):
         name_list = []
@@ -133,17 +158,8 @@ class MaxOp(object):
             name_list.append(obj.name)
         return name_list
 
-    def getObjects(self):
-        return self.objects
-
-    def getSelection(self):
-        return self.selection
-
-    def getAllObjectNames(self):
-        return self.object_names
-
-    def getSelectionNames(self):
-        return self.selection_names
+    def getPolygonCount(self, obj):
+        return rt.getPolygonCount(obj)
 
     def outputNames(self, names, info_level=0):
         line_start = "\t" * info_level
@@ -182,11 +198,16 @@ class MaxOp(object):
 def demo():
     max_op = MaxOp()
 
-    max_op.createBox()
+    #  max_op.createBox()
     max_op.outputInfo()
-    max_op.selectObjectByName("Box001")
+
+    max_op.selectObject("Box003")
     max_op.outputInfo()
-    max_op.deSelectObjectByName("Box001")
+    max_op.selectObject("Box004")
+    max_op.outputInfo()
+    max_op.deSelectObject("Box003")
+    max_op.outputInfo()
+    max_op.deSelectObject("Box004")
     max_op.outputInfo()
 
     print("========")
