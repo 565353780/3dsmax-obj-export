32d9b135b31acdd348ae5ad6844bbd26fd5413c7
chLi <565353780@qq.com>
2022-08-29 12:22:16 +0800

    remove unused path

--- demo.py
+++ demo.py
@@ -4,15 +4,12 @@
 from max_obj_export.Module.max_client import MaxClient
 
 if __name__ == "__main__":
-    server_ip = "192.168.2.16"
-    server_ip = "127.0.0.1"
+    server_ip = "192.168.2.15"
     server_port = 9365
 
     #  max_file_path = "C:/Program Files/Autodesk/3ds Max 2023/presets/Particle Flow/earth_Squib Sand01.max"
     max_file_path = "/home/chli/chLi/tmp.max"
     save_obj_file_path = "/home/chli/chLi/test.obj"
-    max_file_path = "L:/tmp.max"
-    save_obj_file_path = "L:/test.obj"
 
     max_client = MaxClient(server_ip, server_port)
     max_client.transMaxToObj(max_file_path, save_obj_file_path)
